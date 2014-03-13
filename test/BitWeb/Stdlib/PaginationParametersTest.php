088c3164837117a6042b6e03f37e5faa758bbc29
Rain Ramm <rammrain@gmail.com>
2014-03-14 00:42:45 +0200

    All tests for PaginationParameters

--- test/BitWeb/Stdlib/PaginationParametersTest.php
+++ test/BitWeb/Stdlib/PaginationParametersTest.php
@@ -24,4 +24,78 @@ class PaginationParametersTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(PaginationParameters::ORDER_ASC, $paginationParameters->getOrder());
         $this->assertEquals(PaginationParameters::ITEM_COUNT_PER_PAGE, $paginationParameters->getItemCountPerPage());
     }
+
+    public function testItemCountPerPageDefault()
+    {
+        $paginationParameters = new PaginationParameters(null, null, null, null);
+        $this->assertEquals(PaginationParameters::ITEM_COUNT_PER_PAGE, $paginationParameters->getItemCountPerPage());
+    }
+
+    public function testSetAndGetPageNumber()
+    {
+        $paginationParameters = new PaginationParameters();
+        $pageNumber = 2;
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setCurrentPageNumber($pageNumber));
+        $this->assertEquals($pageNumber, $paginationParameters->getCurrentPageNumber());
+    }
+
+    public function testSetAndGetSort()
+    {
+        $paginationParameters = new PaginationParameters();
+        $sort = 'sort';
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setSort($sort));
+        $this->assertEquals($sort, $paginationParameters->getSort());
+    }
+
+    public function testSetAndGetOrder()
+    {
+        $paginationParameters = new PaginationParameters();
+        $order = PaginationParameters::ORDER_DESC;
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setOrder($order));
+        $this->assertEquals($order, $paginationParameters->getOrder());
+    }
+
+    public function testSetAndGetItemCountPerPage()
+    {
+        $paginationParameters = new PaginationParameters();
+        $itemCountPerPage = 100;
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setItemCountPerPage($itemCountPerPage));
+        $this->assertEquals($itemCountPerPage, $paginationParameters->getItemCountPerPage());
+    }
+
+    public function testSetAndGetFilters()
+    {
+        $paginationParameters = new PaginationParameters();
+        $filters = array(
+            'test' => 'value',
+            'key' => 'values'
+        );
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setFilters($filters));
+        $this->assertEquals($filters, $paginationParameters->getFilters());
+    }
+
+    public function testSetAndGetFilter()
+    {
+        $paginationParameters = new PaginationParameters();
+        $key = 'test';
+        $value = 'value';
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setFilter($key, $value));
+        $this->assertEquals($value, $paginationParameters->getFilter($key));
+        $this->assertNull($paginationParameters->getFilter('newKey'));
+    }
+
+    public function testSetAndGetCache()
+    {
+        $paginationParameters = new PaginationParameters();
+        $cache = 'cache';
+
+        $this->assertInstanceOf(PaginationParameters::class, $paginationParameters->setCache($cache));
+        $this->assertEquals($cache, $paginationParameters->getCache());
+    }
 }
