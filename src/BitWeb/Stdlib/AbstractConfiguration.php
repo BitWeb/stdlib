797569388bdeddac8392610a3cebb6b3c120fd61
Rain Ramm <rammrain@gmail.com>
2014-04-15 18:01:47 +0300

    Add abstract configuration

---
+++ src/BitWeb/Stdlib/AbstractConfiguration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BitWeb\Stdlib;
+
+abstract class AbstractConfiguration
+{
+    public function __construct($config = null)
+    {
+        if (null !== $config) {
+            if (is_array($config)) {
+                $this->processArray($config);
+            } else {
+                throw new \InvalidArgumentException(
+                    'Parameter to ' . get_class($this) . ' \'s constructor must be an array'
+                );
+            }
+        }
+    }
+
+    protected function processArray($config)
+    {
+        foreach ($config as $key => $value) {
+            $setter = $this->assembleSetterNameFromConfigKey($key);
+            $this->{$setter}($value);
+        }
+    }
+
+    protected function assembleSetterNameFromConfigKey($key)
+    {
+        $parts = explode('_', $key);
+        $parts = array_map('ucfirst', $parts);
+        $setter = 'set' . implode('', $parts);
+        if (!method_exists($this, $setter)) {
+            throw new \BadMethodCallException(
+                'The configuration key "' . $key . '" does not have a matching ' . $setter . ' setter method which must be defined'
+            );
+        }
+        return $setter;
+    }
+} 
